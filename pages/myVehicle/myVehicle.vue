ac903770eafbb75373400ee80b59b075abe3c879
impHub <yujiarui2009@qq.com>
2020-10-11 12:16:52 +0800

    我的车辆

--- pages/myVehicle/myVehicle.vue
+++ pages/myVehicle/myVehicle.vue
@@ -1,19 +1,39 @@
 <template>
-	<view>
-		我的车辆
+  <view class="wrap">
+    <u-cell-group>
+      <u-cell-item icon="calendar" title="车牌号"></u-cell-item>
+      <u-cell-item icon="car" title="汽车品牌" value="新版本"></u-cell-item>
+      <u-button class="wrap_del" type="error">删除</u-button>
+    </u-cell-group>
+	<view class="wrap_add">
+		<u-button type="success" @click="add">添加</u-button>
 	</view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+export default {
+  data() {
+    return {};
+  },
+  onShow() {
+    this.$u.auth(this.$route.fullPath);
+  },
+  onBackPress() {
+    this.$u.auth(this.$route.fullPath);
+  },
+};
 </script>
 
 <style lang="scss">
-
+.wrap{
+	// padding: 20rpx;
+	.wrap_del{
+		width: 90%;
+	}
+	.wrap_add{
+		width: 20%;
+		margin-top: 100rpx;
+	}
+}
 </style>
